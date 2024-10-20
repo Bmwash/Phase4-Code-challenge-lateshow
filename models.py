5ef3e7ea94f6cce11d5e8ca94ff77a263b39264e
bernard <benmwash700@gmail.com>
2024-10-20 15:10:35 +0300

    updates

--- models.py
+++ models.py
@@ -1,9 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from marshmallow import ValidationError
 
-
 db = SQLAlchemy()
-
 
 class Episode(db.Model):
     __tablename__ = 'episodes'
@@ -12,13 +10,15 @@ class Episode(db.Model):
     number = db.Column(db.Integer, nullable=False)
     appearances = db.relationship('Appearance', back_populates='episode', cascade="all, delete-orphan")
 
-    def to_dict(self):
-        return {
+    def to_dict(self, include_appearances=True):
+        episode_dict = {
             "id": self.id,
             "date": self.date,
             "number": self.number,
-            "appearances": [appearance.to_dict() for appearance in self.appearances]
         }
+        if include_appearances:
+            episode_dict["appearances"] = [appearance.to_dict(include_episode=False) for appearance in self.appearances]
+        return episode_dict
 
 class Guest(db.Model):
     __tablename__ = 'guests'
@@ -27,12 +27,15 @@ class Guest(db.Model):
     occupation = db.Column(db.String, nullable=False)
     appearances = db.relationship('Appearance', back_populates='guest', cascade="all, delete-orphan")
 
-    def to_dict(self):
-        return {
+    def to_dict(self, include_appearances=True):
+        guest_dict = {
             "id": self.id,
             "name": self.name,
             "occupation": self.occupation
         }
+        if include_appearances:
+            guest_dict["appearances"] = [appearance.to_dict(include_guest=False) for appearance in self.appearances]
+        return guest_dict
 
 class Appearance(db.Model):
     __tablename__ = 'appearances'
@@ -44,15 +47,18 @@ class Appearance(db.Model):
     episode = db.relationship('Episode', back_populates='appearances')
     guest = db.relationship('Guest', back_populates='appearances')
 
-    def to_dict(self):
-        return {
+    def to_dict(self, include_episode=True, include_guest=True):
+        appearance_dict = {
             "id": self.id,
             "rating": self.rating,
             "episode_id": self.episode_id,
-            "guest_id": self.guest_id,
-            "episode": self.episode.to_dict(),
-            "guest": self.guest.to_dict(),
+            "guest_id": self.guest_id
         }
+        if include_episode:
+            appearance_dict["episode"] = {"id": self.episode.id, "number": self.episode.number}
+        if include_guest:
+            appearance_dict["guest"] = {"id": self.guest.id, "name": self.guest.name}
+        return appearance_dict
 
     @staticmethod
     def validate_rating(value):
